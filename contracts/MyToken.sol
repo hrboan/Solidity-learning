9ee7d73f48f3fa6c7a925300a7f1a4f9b7cf35e1
hrboan <hrboan@ajou.ac.kr>
2026-04-14 21:38:06 +0900

    added transfer

---
+++ contracts/MyToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals; // uint8 --> 8 bit unsigned int, uint16, ... , uint256
+
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimal, uint256 _amount) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimal;
+        _mint(_amount * 10 ** uint256(decimals), msg.sender); // 1 MT
+    }
+
+    function _mint(uint256 amount, address owner) internal {
+        totalSupply += amount;
+        balanceOf[owner] += amount;
+    }
+
+    function transfer(uint256 amount, address to) external {
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+    }
+}
