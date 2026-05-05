657bbd2eae0044eccee7767a994b29acc8d61567
hrboan <hrboan@ajou.ac.kr>
2026-05-05 22:06:04 +0900

    add reward logic

--- contracts/TinyBank.sol
+++ contracts/TinyBank.sol
@@ -5,24 +5,51 @@ interface IMyToken {
     function transfer(uint256 amount, address to) external;
 
     function transferFrom(address from, address to, uint256 amount) external;
+
+    function mint(uint256 amount, address owner) external;
+
 }
 
 contract TinyBank {
-    event Staked(address, uint256);
+    event Staked(address from, uint256 amount);
+    event Withdraw(uint256 amount, address to);
 
     IMyToken public stakingToken;
+
     mapping(address => uint256) public staked;
     uint256 public totalStaked;
+
+    mapping(address => uint256) public lastClaimedBlock;
+    uint rewardPerBlock = 1 *10**18;
 
     constructor(IMyToken _stakingToken) {
         stakingToken = _stakingToken;
     }
 
+    function distributeReward(address to) internal {
+        if (totalStaked > 0) {
+            uint256 blocks = block.number - lastClaimedBlock[to];
+            uint256 reward = (blocks * rewardPerBlock * staked[to]) / totalStaked;
+            stakingToken.mint(reward, to);
+        }
+        lastClaimedBlock[to] = block.number;
+    }
+
     function stake(uint256 _amount) external {
-        require(_amount >=0, "cannot stake 0 amount");
-        stakingToken.transferFrom(msg.sender, address(this), _amount);
+        require(_amount >= 0, "cannot stake 0 amount"); 
+        distributeReward(msg.sender);
+        stakingToken.transferFrom(msg.sender, address(this), _amount);     
         staked[msg.sender] += _amount;
         totalStaked += _amount;
         emit Staked(msg.sender, _amount);
+    }
+
+    function withdraw(uint256 _amount) external {
+        require(staked[msg.sender] >= _amount, "insufficient staked token");
+        distributeReward(msg.sender);
+        stakingToken.transfer(_amount, msg.sender);
+        staked[msg.sender] -= _amount;
+        totalStaked -= _amount;
+        emit Withdraw(_amount, msg.sender);
     }
 }
